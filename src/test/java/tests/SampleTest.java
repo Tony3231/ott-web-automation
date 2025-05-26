a0a13c74f26333e5dd850955782866da956b1301
root <root@ip-172-31-24-175.eu-north-1.compute.internal>
2025-05-26 14:41:54 +0000

    Add SampleTest class

---
+++ src/test/java/tests/SampleTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test
+    public void testMethod() {
+        System.out.println("SampleTest is running!");
+    }
+}
+
